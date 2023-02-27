f86cc14a51c8a78a5a5e9c0da486e9dfb580a0d9
ziggoon <zackmusgrave20@gmail.com>
2023-02-26 18:18:51 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,114 @@
+use std::io;
+
+use actix_web::{
+    body::BoxBody,
+    dev::ServiceResponse,
+    get,
+    http::{header::ContentType, StatusCode},
+    middleware::{ErrorHandlerResponse, ErrorHandlers},
+    web, App, HttpResponse, HttpServer, Result,
+};
+use handlebars::Handlebars;
+use serde_json::json;
+
+#[derive(Serialize)]
+struct LoginTemplateData {
+    error: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct LoginForm {
+    username: String,
+    password: String,
+}
+
+#[get("/login")]
+async fn get_login_page(hb: web::Data<Handlebars<'_>>) -> Result<HttpResponse> {
+    let data = LoginTemplateData { error: None };
+    let html = hb.render("login", &data)?;
+    Ok(HttpResponse::Ok().body(html))
+}
+
+async fn post_login_form(form: web::Form<LoginForm>) -> Result<HttpResponse> {
+    // Check username and password against database or other authentication mechanism
+    // ...
+
+    // If login fails, redirect back to login page with error message
+    let error = Some("Invalid username or password".to_string());
+    let query_string = serde_urlencoded::to_string(LoginTemplateData { error })?;
+    let url = format!("/login?{}", query_string);
+    Ok(HttpResponse::SeeOther().header("Location", url).finish())
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    // Handlebars uses a repository for the compiled templates. This object must be
+    // shared between the application threads, and is therefore passed to the
+    // Application Builder as an atomic reference-counted pointer.
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./templates")
+        .unwrap();
+
+    let handlebars_ref = web::Data::new(handlebars);
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(error_handlers())
+            .app_data(handlebars_ref.clone())
+            .service(get_login_page)
+            .service(web::resource("/login").route(web::post().to(post_login_form)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+// Custom error handlers, to return HTML responses when an error occurs.
+fn error_handlers() -> ErrorHandlers<BoxBody> {
+    ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found)
+}
+
+// Error handler for a 404 Page not found error.
+fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<BoxBody>> {
+    let response = get_error_response(&res, "Page not found");
+    Ok(ErrorHandlerResponse::Response(ServiceResponse::new(
+        res.into_parts().0,
+        response.map_into_left_body(),
+    )))
+}
+
+// Generic error handler.
+fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> HttpResponse<BoxBody> {
+    let request = res.request();
+
+    // Provide a fallback to a simple plain text response in case an error occurs during the
+    // rendering of the error page.
+    let fallback = |e: &str| {
+        HttpResponse::build(res.status())
+            .content_type(ContentType::plaintext())
+            .body(e.to_string())
+    };
+
+    let hb = request
+        .app_data::<web::Data<Handlebars>>()
+        .map(|t| t.get_ref());
+    match hb {
+        Some(hb) => {
+            let data = json!({
+                "error": error,
+                "status_code": res.status().as_str()
+            });
+            let body = hb.render("error", &data);
+
+            match body {
+                Ok(body) => HttpResponse::build(res.status())
+                    .content_type(ContentType::html())
+                    .body(body),
+                Err(_) => fallback(error),
+            }
+        }
+        None => fallback(error),
+    }
+}
+
